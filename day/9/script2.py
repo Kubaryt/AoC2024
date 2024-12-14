247d893c585de3eb2028cf356535ca2b5515ead0
kubaryt <kubastarzyk6554@gmail.com>
2024-12-14 15:03:43 +0100

    latest version of day 9 part 2, I gave up

--- day/9/script2.py
+++ day/9/script2.py
@@ -5,8 +5,6 @@ def get_input(file_name: str) -> list[tuple[str, int] | tuple[int, int]]:
     with open(file_name) as file:
         _blocks = []
         id = 0
-        block = ""
-        dots = ""
 
         for i, char in enumerate(file.readline().strip()):
             if i % 2 == 0:
@@ -14,10 +12,7 @@ def get_input(file_name: str) -> list[tuple[str, int] | tuple[int, int]]:
                 _blocks.append((id, int(char)))
                 id += 1
             else:
-                dots += "." * int(char)
-                if dots != "":
-                    _blocks.append((".", len(dots)))
-                    dots = ""
+                _blocks.append((".", len("." * int(char))))
 
         return _blocks
 
@@ -30,32 +25,42 @@ def create_map(
         if block[0] == ".":
             continue
         else:
-            try:
-                dot_on_left = None
-                if i == 0:
-                    temp_map = _file_system_map
-                    temp_map = temp_map[::-1]
+            dot_on_left = None
+            if i == 0:
+                temp_map = _file_system_map
+                temp_map = temp_map[::-1]
+            else:
+                temp_map = _file_system_map[::-1]
+                temp_map = temp_map[i:]
+            for j, value in enumerate(temp_map[::-1]):
+                if value[0] == ".":
+                    if value[1] >= block[1]:
+                        dot_on_left = j
+                        break
+            if dot_on_left is not None:
+                block_index = _file_system_map.index(block)
+                if _file_system_map[block_index - 1][0] == "." or _file_system_map[block_index + 1][0] == ".":
+                    if _file_system_map[block_index - 1][0] == ".":
+                        while _file_system_map[block_index - 1][0] == ".":
+                            _file_system_map[block_index] = (".", block[1] + _file_system_map[block_index - 1][1])
+                            del _file_system_map[block_index - 1]
+                            block_index -= 1
+                    elif _file_system_map[block_index + 1][0] == ".":
+                        while _file_system_map[block_index + 1][0] == ".":
+                            _file_system_map[block_index] = (".", block[1] + _file_system_map[block_index + 1][1])
+                            del _file_system_map[block_index + 1]
                 else:
-                    temp_map = _file_system_map[::-1]
-                    temp_map = temp_map[i:]
-                for j, value in enumerate(temp_map[::-1]):
-                    if value[0] == ".":
-                        if value[1] >= block[1]:
-                            dot_on_left = j
-                            break
-                if dot_on_left:
-                    _file_system_map[_file_system_map.index(block)] = (".", block[1])
-                    if _file_system_map[dot_on_left][1] == block[1]:
-                        _file_system_map[dot_on_left] = block
-                    else:
-                        _file_system_map.insert(dot_on_left, block)
-                        _file_system_map[dot_on_left + 1] = (
-                            ".",
-                            _file_system_map[dot_on_left + 1][1] - block[1],
-                        )
-            except IndexError:
-                break
-
+                    _file_system_map[block_index] = (".", block[1])
+                if _file_system_map[dot_on_left][1] == block[1]:
+                    # print("dziengiel", block)
+                    _file_system_map[dot_on_left] = block
+                else:
+                    # print("dziengiel", block)
+                    _file_system_map.insert(dot_on_left, block)
+                    _file_system_map[dot_on_left + 1] = (
+                        ".",
+                        _file_system_map[dot_on_left + 1][1] - block[1],
+                    )
     return _file_system_map
 
 
@@ -69,6 +74,7 @@ def calculate_checksum(
             i += char[1]
             continue
         for _ in range(int(char[1])):
+            print(i, char[0], i * char[0])
             total += i * char[0]
             i += 1
 
@@ -78,6 +84,7 @@ def calculate_checksum(
 if __name__ == "__main__":
     blocks = get_input("input")
     print(blocks)
+    print("=====================================================")
     file_system_map = create_map(blocks)
     print(file_system_map)
     print(calculate_checksum(file_system_map))
